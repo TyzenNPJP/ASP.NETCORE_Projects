be87f7bd83130b99fbf8a71866095bed4b9d31b1
Tyzen <85425438+TyzenNPJP@users.noreply.github.com>
2024-01-24 10:30:44 +0545

    Partial view enhanced

--- DNCP8 - Partial Views/obj/Debug/net6.0/DNCP8_PartialViews.AssemblyInfo.cs
+++ DNCP8 - Partial Views/obj/Debug/net6.0/DNCP8_PartialViews.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DNCP8_PartialViews")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ec55e110d20d027e53569352e63a9017080651dc")]
 [assembly: System.Reflection.AssemblyProductAttribute("DNCP8_PartialViews")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DNCP8_PartialViews")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
